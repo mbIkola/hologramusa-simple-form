fa0ac9f0829ab3f6407774a01634b7049dd353b3
Nickolay Kharchevin <kharchevyn@gmail.com>
2017-11-27 01:35:41 +0200

    docker run script for childs of nature

---
+++ docker.start.example.sh
@@ -0,0 +1,2 @@
+docker run -e MONGO_HOST=127.0.0.1 -e MONGO_DB=holocallcentre -e DEBUG=sales-invest-form:server -e HOST=0.0.0.0 -e SMTP_HOST=bla -e SMTP_USER=blu -e SMTP_PASS=ble -e SMTP_FROM=blyuat -p 127.0.0.12:3000:3000 hologram-callcentre
+
