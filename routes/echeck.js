446db94305280c07b7208563d524464a92bc188b
Nickolay Kharchevin <kharchevyn@gmail.com>
2018-01-20 20:54:39 +0200

    Updated node-greenmoney to latest version.

--- routes/echeck.js
+++ routes/echeck.js
@@ -15,10 +15,25 @@ router.post('/', function(req, res) {
 //   console.log("GOT POST Request", greenmoney);
    greenmoney.oneTimeDraftRTV(
        req.body.name, req.body.phone, req.body.address1, req.body.city, req.body.state,
-       req.body.zip, req.body.routingNumber, req.body.accountNumber, req.body.bankName, req.body.checkAmount
+       req.body.zip, req.body.routingNumber, req.body.accountNumber, req.body.bankName, req.body.checkAmount,
+       {
+           EmailAddress : req.body.email || '',
+           CheckMemo: '',
+           CheckDate: '',
+           CheckNumber: '',
+           PhoneExtension: '',
+           Country: 'US' ,
+           Address2: req.body.address2 || '',
+       }
+
    ).then((e) => {
-       console.log("Success:::", e);
-       res.status(200).json(e);
+       console.log("Success?:::", e);
+       if (  (e.ResultDescription && e.ResultDescription.match(/invalid/i)) ||
+             (e.VerifyResultDescription && e.VerifyResultDescription.match(/not accepted/i))) {
+           res.status(400).json(e.toObject())
+       } else {
+           res.status(200).json(e.toObject());
+       }
    })
    .catch(err => {
        console.error("Form Rejected," + err.message);
