389dd361eea5ca11ca88e61411b3e72b587f4648
Nickolay Kharchevin <kharchevyn@gmail.com>
2018-01-22 20:09:56 +0000

    CheckDate is mandatory field

--- routes/echeck.js
+++ routes/echeck.js
@@ -12,14 +12,19 @@ router.get('/', function(req, res) {
 
 
 router.post('/', function(req, res) {
-//   console.log("GOT POST Request", greenmoney);
+   console.log("GOT POST Request", req.body); ///ereenmoney);
+   let d = new Date();
+   let todayDate = ('0' + (d.getMonth() + 1)).substr(-2) + '/' + ('0' + d.getDate()).substr(-2) + '/' + d.getFullYear();
+   try { 
+   let str = req.body.phone.replace(/-/ig, '');
+   req.body.phone = str.slice(0,3) + '-' + str.slice(3,6) + '-' + str.slice(6);
    greenmoney.oneTimeDraftRTV(
        req.body.name, req.body.phone, req.body.address1, req.body.city, req.body.state,
        req.body.zip, req.body.routingNumber, req.body.accountNumber, req.body.bankName, req.body.checkAmount,
        {
            EmailAddress : req.body.email || '',
-           CheckMemo: '',
-           CheckDate: '',
+           CheckMemo: 'Hologram USA Investment',
+           CheckDate: todayDate, //(d.getMonth()+1)  + '/' + d.getDate() + '/' + d.getFullYear() , ///03/03/2018',
            CheckNumber: '',
            PhoneExtension: '',
            Country: 'US' ,
@@ -36,9 +41,13 @@ router.post('/', function(req, res) {
        }
    })
    .catch(err => {
-       console.error("Form Rejected," + err.message);
+       console.error("Form Rejected,", err);
        res.status(500).json( {message: err.message});
    } );
+   } catch( err ) {
+   	console.error("Error: ", err);
+	res.status(500).json( { message: err.message } );
+   }
 });
 
 
